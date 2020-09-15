b118488e1ebd1a85c4b38f2bade757120388b300
eldad giladi <eldad@frontegg.com>
2020-09-15 21:43:33 +0300

    [FR-200] add support in fast_api

--- test-fast-api.py
+++ test-fast-api.py
@@ -6,8 +6,8 @@ import uvicorn
 app = FastAPI()
 
 
-client_id = '93447df4-edcc-45e5-8664-9fb8c196cf44'
-api_key = '71e8ca5a-3786-4d1b-8fcd-a111d07c7bc2'
+client_id = 'my-client-id'
+api_key = 'my-api-key'
 
 frontegg.init_app(app, client_id=client_id, api_key=api_key, with_secure_access=True)
 
