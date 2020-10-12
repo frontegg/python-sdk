56b97e662b054a5a11d8623ce7911c8eb3c8452f
eldad giladi <eldad@frontegg.com>
2020-10-12 14:44:50 +0300

    fixing context provider to not throw error if authorization header is missing

--- frontegg/flask/contextProvider.py
+++ frontegg/flask/contextProvider.py
@@ -3,6 +3,10 @@ import frontegg.flask
 
 
 
-def context_provider(req):
-    decoded = frontegg.flask.frontegg.decode_jwt(verify=False)
-    return FronteggContext(decoded.get('sub'), decoded.get('tenantId'))
+def context_provider(request):
+    if request.headers.get('Authorization'):
+        decoded = frontegg.flask.frontegg.decode_jwt(verify=False)
+        return FronteggContext(decoded.get('sub'), decoded.get('tenantId'))
+
+    return FronteggContext('user-id', 'tenant-id')
+
