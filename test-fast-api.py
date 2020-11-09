18b9e7f93ab87847740916a360c345874372468d
amitripshtos <amit@noogata.com>
2020-11-09 18:53:10 +0200

    Improved the fastapi dependencies classes and added the ability to check permissions for an endpoint.

--- test-fast-api.py
+++ test-fast-api.py
@@ -1,5 +1,5 @@
 from frontegg.fastapi import frontegg
-from frontegg.fastapi.secure_access import get_user, User
+from frontegg.fastapi.secure_access import FronteggSecurity, User
 from fastapi import FastAPI, Depends
 import uvicorn
 
@@ -13,7 +13,12 @@ frontegg.init_app(app, client_id=client_id, api_key=api_key, with_secure_access=
 
 
 @app.get("/")
-def read_root(user: User = Depends(get_user)) -> User:
+def read_root(user: User = Depends(FronteggSecurity())) -> User:
+    return user
+
+
+@app.get("/authorized")
+def read_root(user: User = Depends(FronteggSecurity(permissions=['my-permission']))) -> User:
     return user
 
 
