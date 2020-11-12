828bc8938eaa274532b2cce57d17b53d2ffe5d53
Aviad Mizrachi <aviadm@gmail.com>
2020-11-12 23:35:21 +0200

    fixes #28 - no default values on withAuthentication

--- frontegg/flask/withAuthentication.py
+++ frontegg/flask/withAuthentication.py
@@ -4,20 +4,29 @@ from . import frontegg
 from flask import abort, Response
 
 
-
 def with_authentication(
-        permission_keys: typing.Optional[list] = [],
-        role_keys: typing.Optional[list] = []
+        permission_keys: typing.Optional[list] = None,
+        role_keys: typing.Optional[list] = None
 ):
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
             try:
                 decoded = frontegg.decode_jwt()
-                valid_permissions = all(
-                    permission in decoded['permissions'] for permission in permission_keys)
-                valid_roles = all(role in decoded['roles']
-                                  for role in role_keys)
+
+                # Initially
+                valid_permissions = True
+                valid_roles = True
+
+                # Validate roles
+                if (role_keys != None):
+                    valid_roles = any(
+                        role in decoded['roles'] for role in role_keys)
+
+                if (permission_keys != None):
+                    valid_permissions = any(
+                        permission in decoded['permissions'] for permission in permission_keys)
+
                 if valid_permissions and valid_roles:
                     return f(*args, **kwargs)
                 abort(403)
