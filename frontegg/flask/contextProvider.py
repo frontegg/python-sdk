783af76ce3677ebfbe8a55f82bd0f0b7c2654689
eldad giladi <eldad@frontegg.com>
2020-11-16 15:27:50 +0200

    fix flask context provider

--- frontegg/flask/contextProvider.py
+++ frontegg/flask/contextProvider.py
@@ -2,7 +2,7 @@ from frontegg import FronteggContext
 import frontegg.flask
 
 def context_provider(request):
-    if request.get('user'):
+    if hasattr(request, 'user'):
         return FronteggContext(request.user.get('sub'), request.user.get('tenantId'))
 
     return FronteggContext('user-id', 'tenant-id')
