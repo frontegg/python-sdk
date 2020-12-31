e24bc85db816fbeb5fe8b40813af536658eb4429
amitripshtos <amit@noogata.com>
2020-12-31 08:59:02 +0200

    Fixed frontegg's requests blocking the FastAPI server due to middleware issues in frontegg.

--- frontegg/fastapi/frontegg.py
+++ frontegg/fastapi/frontegg.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Request, Response
 from frontegg.baseConfig.frontegg_proxy import FronteggProxy
 import typing
 import frontegg.fastapi.secure_access as secure_access
+from fastapi.concurrency import run_in_threadpool
+
 
 class Frontegg(FronteggProxy):
     def __init__(self):
@@ -24,19 +26,25 @@ class Frontegg(FronteggProxy):
 
         super(Frontegg, self).__init__(client_id, api_key, context_provider, authentication_middleware, middleware_prefix)
 
-        @app.middleware('http')
-        async def middleware(request: Request, call_next):
-            path = request.url.path
-            if path.startswith(self.middleware_prefix) or path.startswith('/'+self.middleware_prefix):
-                body = await request.body()
-                host = request.headers.get('host') or request.client.host
-                host = host.split(':')[0]
-                response = self.proxy_request(request=request, method=request.method, path=path,
-                                              host=host, body=body, headers=request.headers,
-                                              params=request.query_params)
-                return Response(content=response.content, status_code=response.status_code, headers=response.headers)
-            else:
-                response = await call_next(request)
-                return response
+        @app.api_route(
+            path="/" + self.middleware_prefix + "{application_path:path}",
+            methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+            include_in_schema=False,
+        )
+        async def proxy_application(
+                application_path: str,
+                request: Request,
+        ) -> Response:
+            body = await request.body()
+            host = request.headers.get('host') or request.client.host
+            host = host.split(':')[0]
+            response = await run_in_threadpool(
+                self.proxy_request,
+                request=request, method=request.method, path=application_path,
+                host=host, body=body, headers=request.headers,
+                params=request.query_params
+            )
+            return Response(content=response.content, status_code=response.status_code, headers=response.headers)
+
 
 frontegg = Frontegg()
